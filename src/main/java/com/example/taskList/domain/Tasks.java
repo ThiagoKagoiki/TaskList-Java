c9c8c91bec83c471df93b4f93470104f34a7be5a
ThiagoKagoiki <thiagoyck@gmail.com>
2025-06-11 21:38:43 -0300

    getters and setters

--- src/main/java/com/example/taskList/domain/Tasks.java
+++ src/main/java/com/example/taskList/domain/Tasks.java
@@ -2,4 +2,31 @@ package com.example.taskList.domain;
 
 public class Tasks {
     //nesta pasta voce criara o objeto task com os getters e setters
+    private String name;
+    private boolean stats;
+    private String desc;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isStats() {
+        return stats;
+    }
+
+    public void setStats(boolean stats) {
+        this.stats = stats;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
 }
